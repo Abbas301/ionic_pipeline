8dd1c90092acf346108804b0b72acf5e4f14688e
abbas.dudekula.ext <abbas.dudekula.ext@siemens.com>
2022-08-11 12:15:59 +0530

    first commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  
+  constructor() {
+    
+  }
+  ngOnInit(): void {
+   
+    
+  }
+
+  func(){
+    for(let i=0;i<10;i++) {
+
+    }
+  }
+
+  
+  
+
+  
+
+  
 
 }
